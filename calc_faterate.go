2f387c655be0c80eeb0deabfb1a0f98117f55dfd
Jesse Zhang <jesse@tigerwit.com>
2022-01-04 13:59:40 +0800

    添加[]float64类型切片去重模块。

--- calc_faterate.go
+++ calc_faterate.go
@@ -7,7 +7,7 @@ func CalcFatRate(bmi float64, age int, sex string) (fatRate float64) {
 	} else {
 		sexWeight = 0
 	}
-	fatRate = (1.2*bmi + 0.23*float64(age) - 5.4 - (10.8*float64(sexWeight))) / 100
+	fatRate = (1.2*bmi + 0.23*float64(age) - 5.4 - (10.8 * float64(sexWeight))) / 100
 	return
 }
 
---
+++ removeRepeatElement.go
@@ -0,0 +1,30 @@
+package gobmi
+
+import (
+	"fmt"
+	"sort"
+)
+
+func RemoveRepeatElementsForFloat64(oldArray []float64) (newArray []float64) {
+	// 如果切片的元素相同 则我们需要进一步做去重处理
+	sort.Float64s(oldArray)
+
+	for i := 0; i < len(oldArray); i++ {
+		repeatElement := false
+		for j := i + 1; j < len(oldArray); j++ {
+			if newArray[i] == oldArray[j] {
+				repeatElement = true
+				break
+			}
+		}
+
+		if repeatElement == false {
+			newArray = append(newArray, oldArray[i])
+		}
+	}
+
+	//Debug调试
+	fmt.Printf("oldArr:%v\n", oldArray)
+	fmt.Printf("newArr:%v\n", newArray)
+	return
+}
